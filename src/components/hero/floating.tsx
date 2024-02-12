8ec857a743d12e6cd90762f4075e5f9eddb1af3e
praktyki <jactyl13@gmail.com>
2024-02-12 14:54:04 +0100

    Add floating elements

--- src/components/hero/floating.tsx
+++ src/components/hero/floating.tsx
@@ -4,13 +4,16 @@ interface Props {
     url: string,
     posX: number,
     posY: number,
+    scale?: number,
+    rotation?: number
 }
 
-export default function Floating({url, posX, posY}: Props) {
+export default function Floating({url, posX, posY, scale = 1, rotation = 0}: Props) {
     return (
         <Image 
-            src={url} alt="" aria-hidden width={50} height={0}
-            className="absolute top-0"
+            src={url} alt="" aria-hidden width={50*scale} height={0}
+            className='absolute opacity-80 z-0'
+            style={{top: `${posY}%`, left: `${posX}%`, rotate: `${rotation}deg`}}
         />
 
     )
